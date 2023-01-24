f021186a5e3ab0eee174d0226fd7ebb2c00b79de
hunterdyar <bloop@hdyar.com>
2023-01-24 17:56:08 -0500

    fix links, noreferrer

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,9 +11,9 @@ export default function About()
         <Card>
             <CardContent>
                 <Typography>
-                <p>This website was created by <a href="http://hdyar.com" target="_blank">Hunter Dyar</a>, for his students.</p>
+                <p>This website was created by <a href="https://hdyar.com" rel="noreferrer" target="_blank">Hunter Dyar</a>, for his students.</p>
                 <p>All assets on this site were created by Hunter, or by a student worker explicitly doing paid work to create assets for this collection. No student coursework is allowed on the collection, although students are encouraged to use the assets for their own projects.</p>
-                <p>This site was created with <a href="https://reactjs.org/" target="_blank">React</a>, using <a href="https://mui.com/" target="_blank">MUI</a>. It is hosted on <a href="https://github.com/hunterdyar/hunters-asset-db/" target="_blank">Github</a>.</p>
+                <p>This site was created with <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React</a>, using <a href="https://mui.com/" rel="noreferrer" target="_blank">MUI</a>. It is hosted on <a href="https://github.com/hunterdyar/hunters-asset-db/" rel="noreferrer" target="_blank">Github</a>.</p>
                 </Typography>
                 </CardContent>
         </Card>
